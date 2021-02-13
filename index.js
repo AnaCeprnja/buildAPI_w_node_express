5794bc62b1ca9bf440bb5752bbc7b7bd8dd33ef1
Ana Ceprnja <aceprnja@iun.edu>
2021-02-13 11:53:25 -0700

    404 errors added

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const Joi = require('joi');
 const express = require ('express');
 const app = express();
+//middleware
+app.use(express.json());
 
 const courses = [
     {id: 1, name: 'courses1'},
@@ -22,9 +25,54 @@ app.get('/api/courses/:id', (req,res) => {
 });
 
 app.post('/api/courses', (req,res) => {
+    const { error } = validateCourse(req.body);
+    if (error) {
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
+    const course = {
+        id:courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
 
 });
+app.put('/api/courses/:id', (req, res =>) {
+    // look up course
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send() 
+    ///if not there return 404
+    
+    //validate
+    
+    //if invalid reurn 400 error
+   
+   const { error }= validateCourse(req.body);
+    if (error) {
+        res.status(400).send(result.error.details[0].message);
+    return;    }
+    //update course
+    course.name = req.body.name;
+    
+    //return the updated course
+    res.send(course);
+    });
+
+    function validateCourse(course) {
+        const schema = {
+            name: Joi.string().min(3).required()
+        };
+       return Joi.validate(course, schema);
+
+    }
+
+
+
 
 //PORT 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log (`listening on port ${port}`));
+
+
